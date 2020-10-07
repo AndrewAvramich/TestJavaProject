b79ef77343fba51c75042dc7b0afe47623c14ac8
max_kopachevskii <max169665@gmail.com>
2020-10-07 20:25:51 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Sum = " + UtilityMathClass.Sum(5, 5));
+        System.out.println("Dist = " + UtilityMathClass.Dist(10,5));
+        System.out.println("Mult = " + UtilityMathClass.Mult(10,10));
+        System.out.println("Division = " + UtilityMathClass.Division(100,5));
+    }
+}
---
+++ src/UtilityMathClass.java
@@ -0,0 +1,18 @@
+public class UtilityMathClass
+{
+    public static int Sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int Dist(int a, int b) {
+        return a - b;
+    }
+
+    public static int Mult(int a, int b) {
+        return a * b;
+    }
+
+    public static int Division(int a, int b) {
+        return a / b;
+    }
+}
