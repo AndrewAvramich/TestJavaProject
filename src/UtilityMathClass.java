ff151f47f7bd8e3530ae081367dddda781b42b29
Avramych Andrii <avramich10@gmail.com>
2020-10-09 19:51:32 +0300

    added isEven method

--- src/UtilityMathClass.java
+++ src/UtilityMathClass.java
@@ -15,4 +15,11 @@ public class UtilityMathClass
     public static int Division(int a, int b) {
         return a / b;
     }
+
+    public static boolean IsEven(int a){
+        if(a % 2 == 0){
+            return true;
+        }
+        return false;
+    }
 }
